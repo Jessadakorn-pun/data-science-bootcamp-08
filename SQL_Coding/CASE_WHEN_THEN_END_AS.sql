e203a5d2a4d93d96e64650f8a31f9ade0fa23ee3
Jessadakorn-pun <136186528+Jessadakorn-pun@users.noreply.github.com>
2023-10-04 11:24:43 +0700

    Create CASE_WHEN_THEN_END_AS.sql

---
+++ SQL_Coding/CASE_WHEN_THEN_END_AS.sql
@@ -0,0 +1,9 @@
+SELECT
+  country,
+  CASE
+    WHEN country IN ('Canada', 'USA') THEN 'America'
+    WHEN country IN ('Belgium', 'France', 'Italy') THEN 'Europe'
+    ELSE 'Other'
+  END AS region
+FROM customers
+
